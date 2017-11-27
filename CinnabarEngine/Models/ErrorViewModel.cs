6b93a9175e3f0793f5ab6de0e45d0428bd8fa9f7
Cameron Crockrom <ccrockrom@gmail.com>
2017-11-27 16:03:22 -0600

    Create dotnet core project

---
+++ CinnabarEngine/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CinnabarEngine.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
